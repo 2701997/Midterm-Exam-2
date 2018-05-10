9a2dde8d231f148138effa65736bdb598d48ae2e
2701997 <36818921+2701997@users.noreply.github.com>
2018-05-10 10:02:10 +0200

    sort

--- task4.py
+++ task4.py
@@ -1,21 +1,39 @@
-"""
-===================   TASK 4  ====================
-* Name: Merge Sort
-*
-* Write a function that will implement merge sort
-* algorithm. Generate list of 1000 random integer
-* numbers and sort the list using your function.
-*
-* Note: Please describe in details possible cases
-* in which your solution might not work.
-*
-* Use main() function to test your solution.
-===================================================
-"""
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
 
-# Write your function here
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
 
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+alist = [54,26,93,17,77,31,44,55,20]
+mergeSort(alist)
+print(alist)
 
 def main():
     # Test your function here
